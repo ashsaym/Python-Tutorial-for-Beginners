ffd6b14f98f4ee61e6505f32758b367cdc58790d
Saym Ash <abusaym@outlook.de>
2019-06-01 03:26:15 +0200

    adding data types

---
+++ Data Types/data types.py
@@ -0,0 +1,15 @@
+#Numbers
+    #int	 	 	a = 10	        	 	Signed Integer
+    #long	 	 	a = 345L	 	 	(L) Long integers, they can also be represented in octal and hexadecimal
+    #float	 	 	a = 45.67	 	 	(.) Floating point real values
+    #complex	 	a = 3.14J	 	 	(J) Contains integer in the range 0 to 255.
+#String
+#List
+    #A list is a mutable, or changeable, ordered sequence of elements
+    #[-3, -2, -1, 0, 1, 2, 3]
+#Tuple
+    #A tuple is used for grouping data
+    #('blue', 'red', 'green')
+#Dictionary
+    #The dictionary is Python’s built-in mapping type
+    #{'name': 'Saym', 'age': 25, 'sex': 'male', 'location': 'Germany'}
